545b7ad388e32db90d5bf7278eecf2d43ae778e8
Stefano Tota <stefanotota@MBP-di-Stefano.home-life.hub>
2019-07-30 21:36:57 +0200

    Aggiunta del component search-movie

--- film-app/src/app/app.module.ts
+++ film-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchMovieComponent } from './search-movie/search-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMovieComponent
   ],
   imports: [
     BrowserModule,
